f66375d0dd56b7a3185faf2c9bf17703de8d6786
Norush <a007alpum@gmail.com>
2024-10-23 15:22:04 +0800

    fix:  config @ alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path';
+
+const projectRoot = path.resolve(__dirname, '');
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.join(projectRoot, 'src')
+        }
+    },
     server: {
         host: 'localhost',
         port: 8297,
