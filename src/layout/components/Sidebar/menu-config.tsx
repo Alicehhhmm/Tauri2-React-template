76ec20337a959902fa477a0b7e31198717407b68
Norush <a007alpum@gmail.com>
2024-10-25 15:51:17 +0800

    fix: add icon

---
+++ src/layout/components/Sidebar/menu-config.tsx
@@ -0,0 +1,19 @@
+import { VscHome, VscFeedback, VscBeaker } from 'react-icons/vsc'
+
+export const MENU_NAV_MAP = [
+    {
+        path: '/home',
+        label: 'Home',
+        icon: <VscHome />,
+    },
+    {
+        path: '/exp',
+        label: 'Example',
+        icon: <VscBeaker />,
+    },
+    {
+        path: '/about',
+        label: 'About',
+        icon: <VscFeedback />,
+    },
+]
